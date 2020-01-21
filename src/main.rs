16f1929c91fecb36884c19ef0678477c67e9df24
Takashi, Murata <murata.muu@gmail.com>
2020-01-22 00:07:47 +0900

    Move Config structure to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-#[macro_use]
-extern crate clap;
+extern crate hello;
+
+use hello::Config;
 
 use tokio::net::TcpListener;
 use tokio_util::codec::{BytesCodec, Decoder};
@@ -29,33 +30,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             println!("Socket received FIN packet and closed connection");
         });
-    }
-}
-
-struct Config {
-    listen_port: u16,
-}
-
-impl Config {
-    fn parse_cmd_line() -> Result<Config, Box<dyn Error>> {
-        use clap::{App, Arg};
-
-        let app = App::new(crate_name!())
-            .version(crate_version!())
-            .author(crate_authors!())
-            .about(crate_description!())
-            .arg(Arg::with_name("listen_port")
-                .help("listening port number")
-                .short("p")
-                .long("port")
-                .takes_value(true)
-        );
-
-        let matches = app.get_matches();
-        let listen_port = matches.value_of("listen_port")
-            .unwrap_or("8080")
-            .parse()?;
-
-        Ok(Config { listen_port })
     }
 }
