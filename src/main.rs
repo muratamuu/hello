0e1218fe460d5514b5f601c7f2aed0cfd00e76f0
Takashi, Murata <murata.muu@gmail.com>
2020-01-21 23:55:29 +0900

    Move command line analysis to Config structure.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{App, Arg};
-
 use tokio::net::TcpListener;
 use tokio_util::codec::{BytesCodec, Decoder};
 use tokio::stream::StreamExt;
@@ -10,21 +8,10 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let app = App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(Arg::with_name("listen_port")
-            .help("listening port number")
-            .short("p")
-            .long("port")
-            .takes_value(true)
-        );
 
-    let matches = app.get_matches();
+    let config = Config::parse_cmd_line()?;
 
-    let listen_port = matches.value_of("listen_port").unwrap_or("8080");
-    let addr = format!("0.0.0.0:{}", listen_port).to_string();
+    let addr = format!("0.0.0.0:{}", config.listen_port).to_string();
 
     let mut listener = TcpListener::bind(&addr).await?;
     println!("Listening on: {}", addr);
@@ -42,5 +29,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             println!("Socket received FIN packet and closed connection");
         });
+    }
+}
+
+struct Config {
+    listen_port: u16,
+}
+
+impl Config {
+    fn parse_cmd_line() -> Result<Config, Box<dyn Error>> {
+        use clap::{App, Arg};
+
+        let app = App::new(crate_name!())
+            .version(crate_version!())
+            .author(crate_authors!())
+            .about(crate_description!())
+            .arg(Arg::with_name("listen_port")
+                .help("listening port number")
+                .short("p")
+                .long("port")
+                .takes_value(true)
+        );
+
+        let matches = app.get_matches();
+        let listen_port = matches.value_of("listen_port")
+            .unwrap_or("8080")
+            .parse()?;
+
+        Ok(Config { listen_port })
     }
 }
