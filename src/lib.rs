4698092a2e42e19aed5d29d44e367855372eadd0
Takashi, Murata <murata.muu@gmail.com>
2020-01-23 23:35:13 +0900

    Move run function to lib.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 extern crate clap;
 
 use std::error::Error;
+use tokio::net::TcpListener;
+use tokio_util::codec::{BytesCodec, Decoder};
+use tokio::stream::StreamExt;
 
 pub struct Config {
     pub listen_port: u16,
@@ -28,5 +31,28 @@ impl Config {
             .parse()?;
 
         Ok(Config { listen_port })
+    }
+}
+
+pub async fn run(config: Config) -> Result<(), Box<dyn Error>> {
+
+    let addr = format!("0.0.0.0:{}", config.listen_port).to_string();
+
+    let mut listener = TcpListener::bind(&addr).await?;
+    println!("Listening on: {}", addr);
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut framed = BytesCodec::new().framed(socket);
+
+            while let Some(message) = framed.next().await {
+                match message {
+                    Ok(bytes) => println!("bytes: {:?}", bytes),
+                    Err(err) => println!("Socket closed with error: {:?}", err),
+                }
+            }
+            println!("Socket received FIN packet and closed connection");
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 extern crate hello;
 
-use hello::Config;
-
-use tokio::net::TcpListener;
-use tokio_util::codec::{BytesCodec, Decoder};
-use tokio::stream::StreamExt;
+use hello::{run, Config};
 use std::error::Error;
 
 #[tokio::main]
@@ -15,27 +11,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
     run(config).await?;
 
     Ok(())
-}
-
-async fn run(config: Config) -> Result<(), Box<dyn Error>> {
-
-    let addr = format!("0.0.0.0:{}", config.listen_port).to_string();
-
-    let mut listener = TcpListener::bind(&addr).await?;
-    println!("Listening on: {}", addr);
-
-    loop {
-        let (socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut framed = BytesCodec::new().framed(socket);
-
-            while let Some(message) = framed.next().await {
-                match message {
-                    Ok(bytes) => println!("bytes: {:?}", bytes),
-                    Err(err) => println!("Socket closed with error: {:?}", err),
-                }
-            }
-            println!("Socket received FIN packet and closed connection");
-        });
-    }
 }
